974e3c285095aa5ff5a998e5a74954b6eb935cdb
Kasper Laurberg <kasperlaurberg@gmail.com>
2015-02-15 12:55:31 +0100

    Added test file

---
+++ Binaries/Debug/TestRef.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace TestRefNamespace
+{
+    class TestRef
+    {
+        private static void Add(int i, int result)
+        {
+            
+            atomic{ 
+                Console.WriteLine("Atomic works");
+            }
+            /*
+            if (true){ 
+                Console.WriteLine("If3 works");
+            }
+            result += 1;*/
+            return;
+        }
+
+        static void Main()
+        {
+            int total = 20;
+            System.Console.WriteLine("Original value of 'total': {0}", total);
+
+            Add(10, total);
+            System.Console.WriteLine("Value after calling Add(): {0}", total);
+        }
+    }
+}
