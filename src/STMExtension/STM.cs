32ca6a24c07a2a4c88693745fb7076da45393c66
Airbagjoe <kasperlaurberg@gmail.com>
2015-04-06 18:30:50 +0200

    Added STM dlls to repo and added support for multiple trees

--- src/STMExtension/STM.cs
+++ src/STMExtension/STM.cs
@@ -18,121 +18,128 @@ namespace STMExtension
             {
                 throw new Exception("There are more than one syntax tree in trees: Undefined behaviour.");
             }
-
-            var tree = trees.First();
-            var root = tree.GetRoot();
-            //var textBefore = root.GetText().ToString(); //Get source text before transformation (for testing and debugging) TestRef.cs
-
-            //replace atomic field types
-            List<FieldDeclarationSyntax> allFields = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.FieldDeclaration)).Cast<FieldDeclarationSyntax>().ToList();
-            var atomicFields = allFields.Where(node => node.Modifiers.Any(SyntaxKind.AtomicKeyword));
-            var fieldDclReplaceDic = new Dictionary<FieldDeclarationSyntax, FieldDeclarationSyntax>();
-
-            foreach (FieldDeclarationSyntax aField in atomicFields)
+            for (int i = 0; i < trees.Length; i++)
             {
-                //Remove atomic from modifier list
-                var newModifierList = aField.Modifiers.Where(mod => !mod.IsKind(SyntaxKind.AtomicKeyword)).ToList();
-                var newFieldDcl = aField.WithModifiers(SyntaxFactory.TokenList(newModifierList));
+                var tree = trees[i];
+                var root = tree.GetRoot();
 
-                //Change declaration type to our TMVar type (or specific like TMInt)
-                VariableDeclarationSyntax aFVarDcl = aField.Declaration;
-                TypeSyntax aFType = aFVarDcl.Type; //original type node
+                //var textBefore = root.GetText().ToString(); //Get source text before transformation (for testing and debugging) TestRef.cs
 
-                string aFTypeStr = "";
-                string aFFullTypeStr = "";
+                //replace atomic field types
+                List<FieldDeclarationSyntax> allFields = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.FieldDeclaration)).Cast<FieldDeclarationSyntax>().ToList();
+                var atomicFields = allFields.Where(node => node.Modifiers.Any(SyntaxKind.AtomicKeyword));
+                var fieldDclReplaceDic = new Dictionary<FieldDeclarationSyntax, FieldDeclarationSyntax>();
 
-                if (aFType.IsKind(SyntaxKind.IdentifierName)) //userdefined types
+                foreach (FieldDeclarationSyntax aField in atomicFields)
                 {
-                    aFTypeStr = ((IdentifierNameSyntax)aFType).Identifier.Text;
-                    aFFullTypeStr = "TMVar<" + aFTypeStr + ">";
-                }
-                else if (aFType.IsKind(SyntaxKind.PredefinedType))
-                {
-                    aFTypeStr = ((PredefinedTypeSyntax)aFType).Keyword.Text;
-                    string aFTypeStrUp = FirstCharToUpper(aFTypeStr);
+                    //Remove atomic from modifier list
+                    var newModifierList = aField.Modifiers.Where(mod => !mod.IsKind(SyntaxKind.AtomicKeyword)).ToList();
+                    var newFieldDcl = aField.WithModifiers(SyntaxFactory.TokenList(newModifierList));
 
-                    switch (aFTypeStr)
+                    //Change declaration type to our TMVar type (or specific like TMInt)
+                    VariableDeclarationSyntax aFVarDcl = aField.Declaration;
+                    TypeSyntax aFType = aFVarDcl.Type; //original type node
+
+                    string aFTypeStr = "";
+                    string aFFullTypeStr = "";
+
+                    if (aFType.IsKind(SyntaxKind.IdentifierName)) //userdefined types
                     {
-                        case "int":
-                        case "long":
-                        case "double":
-                        case "float":
-                        case "uint":
-                        case "ulong":
-                            aFFullTypeStr = "TM" + aFTypeStrUp;
-                            break;
-                        case "string":
-                        default:
-                            aFFullTypeStr = "TMVar<" + aFTypeStrUp + ">";
-                            break;
+                        aFTypeStr = ((IdentifierNameSyntax)aFType).Identifier.Text;
+                        aFFullTypeStr = "TMVar<" + aFTypeStr + ">";
                     }
+                    else if (aFType.IsKind(SyntaxKind.PredefinedType))
+                    {
+                        aFTypeStr = ((PredefinedTypeSyntax)aFType).Keyword.Text;
+                        string aFTypeStrUp = FirstCharToUpper(aFTypeStr);
+
+                        switch (aFTypeStr)
+                        {
+                            case "int":
+                            case "long":
+                            case "double":
+                            case "float":
+                            case "uint":
+                            case "ulong":
+                                aFFullTypeStr = "TM" + aFTypeStrUp;
+                                break;
+                            case "string":
+                            default:
+                                aFFullTypeStr = "TMVar<" + aFTypeStrUp + ">";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("This declaration type is unknown");
+                    }
+
+                    var newTypeDcl = SyntaxFactory.ParseName(aFFullTypeStr + " "); //whitespace needed to seperate type from name
+
+                    //Build new field declaration and add to dic
+                    newFieldDcl = newFieldDcl.WithDeclaration(SyntaxFactory.VariableDeclaration(newTypeDcl, aFVarDcl.Variables));
+                    fieldDclReplaceDic.Add(aField, newFieldDcl);
                 }
-                else
+                root = root.ReplaceNodes(fieldDclReplaceDic.Keys, (oldnode, newnode) => fieldDclReplaceDic[oldnode]);
+
+                //replace atomics and orelses
+                var atomicNodes = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.AtomicStatement)).ToList();
+                var atomicReplaceDic = new Dictionary<AtomicStatementSyntax, ExpressionStatementSyntax>();
+
+                foreach (AtomicStatementSyntax aNode in atomicNodes)
                 {
-                    throw new Exception("This declaration type is unknown");
+                    //Build up arguments to library call
+                    List<ArgumentSyntax> aArguments = new List<ArgumentSyntax>();
+
+                    //Atomic arg
+                    StatementSyntax aBlock = aNode.Statement; //(BlockSyntax)childNodes.ElementAt(0);
+                    var aLambda = SyntaxFactory.ParenthesizedLambdaExpression(aBlock);
+                    var atomicArg = SyntaxFactory.Argument(aLambda);
+                    aArguments.Add(atomicArg);
+
+                    //OrElse args
+                    var aOrElses = aNode.Orelses;
+                    foreach (var oe in aOrElses)
+                    {
+                        var oeLambda = SyntaxFactory.ParenthesizedLambdaExpression(oe.Statement);
+                        var oeArg = SyntaxFactory.Argument(oeLambda);
+                        aArguments.Add(oeArg);
+                    }
+
+                    //Create library call
+                    var atomicInvoNode = SyntaxFactory.ExpressionStatement(
+                    SyntaxFactory.InvocationExpression(
+                        SyntaxFactory.ParseName("STMSystem.Atomic"),
+                        SyntaxFactory.ArgumentList(
+                            arguments: SyntaxFactory.SeparatedList<ArgumentSyntax>(aArguments))));
+
+                    atomicReplaceDic.Add(aNode, atomicInvoNode);
                 }
 
-                var newTypeDcl = SyntaxFactory.ParseName(aFFullTypeStr + " "); //whitespace needed to seperate type from name
+                root = root.ReplaceNodes(atomicReplaceDic.Keys, (oldnode, newnode) => atomicReplaceDic[oldnode]);
 
-                //Build new field declaration and add to dic
-                newFieldDcl = newFieldDcl.WithDeclaration(SyntaxFactory.VariableDeclaration(newTypeDcl, aFVarDcl.Variables));
-                fieldDclReplaceDic.Add(aField, newFieldDcl);
-            }
-            root = root.ReplaceNodes(fieldDclReplaceDic.Keys, (oldnode, newnode) => fieldDclReplaceDic[oldnode]);
-
-            //replace atomics and orelses
-            var atomicNodes = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.AtomicStatement)).ToList();
-            var atomicReplaceDic = new Dictionary<AtomicStatementSyntax, ExpressionStatementSyntax>();
-
-            foreach (AtomicStatementSyntax aNode in atomicNodes)
-            {
-                //Build up arguments to library call
-                List<ArgumentSyntax> aArguments = new List<ArgumentSyntax>();
-
-                //Atomic arg
-                StatementSyntax aBlock = aNode.Statement; //(BlockSyntax)childNodes.ElementAt(0);
-                var aLambda = SyntaxFactory.ParenthesizedLambdaExpression(aBlock);
-                var atomicArg = SyntaxFactory.Argument(aLambda);
-                aArguments.Add(atomicArg);
-
-                //OrElse args
-                var aOrElses = aNode.Orelses;
-                foreach(var oe in aOrElses)
+                //replace retry's
+                var retryReplaceDic = new Dictionary<RetryStatementSyntax, ExpressionStatementSyntax>();
+                var retryNodes = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.RetryStatement)).ToList();
+                foreach (RetryStatementSyntax rNode in retryNodes)
                 {
-                    var oeLambda = SyntaxFactory.ParenthesizedLambdaExpression(oe.Statement);
-                    var oeArg = SyntaxFactory.Argument(oeLambda);
-                    aArguments.Add(oeArg);
+                    var retryInvoNode = SyntaxFactory.ExpressionStatement(
+                        SyntaxFactory.InvocationExpression(SyntaxFactory.ParseName("STMSystem.Retry")));
+                    retryReplaceDic.Add(rNode, retryInvoNode);
                 }
+                root = root.ReplaceNodes(retryReplaceDic.Keys, (oldnode, newnode) => retryReplaceDic[oldnode]);
 
-                //Create library call
-                var atomicInvoNode = SyntaxFactory.ExpressionStatement(
-                SyntaxFactory.InvocationExpression(
-                    SyntaxFactory.ParseName("STMSystem.Atomic"),
-                    SyntaxFactory.ArgumentList(
-                        arguments: SyntaxFactory.SeparatedList<ArgumentSyntax>(aArguments))));
-
-                atomicReplaceDic.Add(aNode, atomicInvoNode);
+                //Create new syntax tree (based on new root), and update it as the current tree in the trees array
+                SyntaxTree newTree = SyntaxFactory.SyntaxTree(root, tree.Options, tree.FilePath);
+                trees[i] = newTree;
+                //Get source text after transformation (for testing and debug purposes)
+                var textAfter = newTree.GetText().ToString();
+                var appendText = "File: " + tree.FilePath + "\n" + textAfter + "\n\n";
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "TextAfterCompilation.txt", appendText);
+                //File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "TextAfterCompilation.txt", textAfter);
             }
 
-            root = root.ReplaceNodes(atomicReplaceDic.Keys, (oldnode, newnode) => atomicReplaceDic[oldnode]);
-
-            //replace retry's
-            var retryReplaceDic = new Dictionary<RetryStatementSyntax, ExpressionStatementSyntax>();
-            var retryNodes = root.DescendantNodes().Where(node => node.IsKind(SyntaxKind.RetryStatement)).ToList();
-            foreach (RetryStatementSyntax rNode in retryNodes)
-            {
-                var retryInvoNode = SyntaxFactory.ExpressionStatement(
-                    SyntaxFactory.InvocationExpression(SyntaxFactory.ParseName("STMSystem.Retry")));
-                retryReplaceDic.Add(rNode, retryInvoNode);
-            }
-            root = root.ReplaceNodes(retryReplaceDic.Keys, (oldnode, newnode) => retryReplaceDic[oldnode]);
-
-            //Create new syntax tree (based on new root), and update it as the current tree in the trees array
-            SyntaxTree newTree = SyntaxFactory.SyntaxTree(root, tree.Options, tree.FilePath);
-            trees[0] = newTree;
-            //Get source text after transformation (for testing and debug purposes)
-            var textAfter = newTree.GetText().ToString();
-            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "TextAfterCompilation.txt", textAfter);
+           
         }
         private static string FirstCharToUpper(string s)
         {
